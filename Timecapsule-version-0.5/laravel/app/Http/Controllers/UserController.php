2fbcc2bd7a6b5e9d1ae3e09155549f890140001d
Vampiercsr <32346755+Vampiercsr@users.noreply.github.com>
2018-03-13 10:00:38 +0800

    框架

---
+++ Timecapsule-version-0.5/laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware(function ($request, $next) {
+            $this->user = $request->user();
+            return $next($request);
+        });
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user=$this->user;
+        // $query =user::where('email','=',$user->email)->firstOrFail();
+
+        return view('user',[
+            'user'=>$user
+        ]);
+    }
+}
